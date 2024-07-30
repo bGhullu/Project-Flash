691a9b3f119d190f23d7c3d1b9b95a6ea081f333
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-07-30 14:50:02 +0100

    AI

--- script/ai.js
+++ script/ai.js
@@ -1,4 +1,17 @@
 const axios = require('axios');
+const winston = require('winston');
+
+const logger = winston.createLogger ({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({timestamp, level, message})=>'${timestamp} ${level}:$ {message}')
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: 'aiService.log'})
+    ]
+});
 
 
 const getAIPrediction = async (features) => {
